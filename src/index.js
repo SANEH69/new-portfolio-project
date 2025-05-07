7c7ce88267fc0b254a6fe10a9871f6911f4eaed0
SANEH69 <sanehthandoh02@gmail.com>
2025-05-07 18:12:17 +0200

    Deleted my JavaScript folder and removed the change theme button

--- src/index.js
+++
@@ -1,7 +0,0 @@
-function darkTheme() {
-    let body = document.querySelector("body");
-    body.classList.toggle("black");
-  }
-
-  let button = document.querySelector("button");
-  button.addEventListener("click", darkTheme)
